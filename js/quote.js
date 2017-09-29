e7b9188573f7baad371987daa2f90592e37973cd
SilverBirchh <bradleybirch09@yahoo.co.uk>
2017-09-29 21:13:37 +0100

    Suppy to quote

--- js/quote.js
+++ js/quote.js
@@ -1,7 +1,42 @@
-// Select all links with hashes
-$('.enquire')
-  .click(function(event) {
-    if (location.pathname === "/supply/") {
-      location.href = "/quote/?type=1"
+// For an enquire click. Could use a class here but nevermind
+jQuery(function($) {
+
+  setQuoteMessageIfApplicable();
+
+  $('.enquire')
+    .click(function(event) {
+      const message = `I would like to enquire about a supply of ${selectOil(event)}.`
+      location.href = `/quote/?message=${message}&type=oil-col`
+    })
+
+  function selectOil(event) {
+    const parent = $(event.target).closest('div');
+    const title = parent.children()[0];
+    return title.innerHTML;
+  }
+
+  function getURLParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : sParameterName[1];
+      }
     }
-  })($);
+  }
+
+  function setQuoteMessageIfApplicable() {
+    if (location.pathname === '/quote/') {
+      let quote = getURLParameter('message');
+      let type = getURLParameter('type');
+
+      $('#message').val(quote);
+      $(`#${type}`).prop('checked', true);
+    }
+  }
+});
